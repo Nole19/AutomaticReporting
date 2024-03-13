70cd70e55f666139287e7f6c919c0a7c09685d9d
Nole19 <hightech1919@gmail.com>
2024-03-13 23:49:06 +0100

    Automatic Reporting

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "title": "node --trace-deprecation",
+  repository: 'https://github.com/Nole19/AutomaticReporting.git',
+  "name": "node_trace_deprecation",
+  "description": "",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "json"
+  }
+}
+
+module.exports = {
+  // other configurations
+  repository: 'https://github.com/<username>/<repository>',
+};
